c846a1ff33cc68f0bb1bfe4bf832ab9523230205
DineshAnnamreddy <144252449+DineshAnnamreddy@users.noreply.github.com>
2023-11-09 21:07:13 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,12 +9,12 @@ const Contact = () => {
             <button onClick={() => { setisContact(!isContact) }} className='text-xl text-white my-3'>Contact <span className='text-base'> {!isContact ? "+" : "-"}</span></button>
             {
                 isContact && <div className='bg-white px-10 py-5 text-start rounded-md'>
-                    <h1>Email:<span className=''>chandua.royal@gmail.com</span></h1>
+                    <h1>Email:<span className=''>dineshannamreddy@gmail.com</span></h1>
                     <h1>Phone:<span className=''>+91 7981979909</span></h1>
                     <div className='flex w-full justify-between items-center px-10 group my-2'>
                         <a href='#'><BiLogoTwitter style={{ fontSize: '18px' }} /></a>
                         <a href='https://www.linkedin.com/in/achanduroyal/'><BiLogoLinkedin style={{ fontSize: '18px' }} /></a>
-                        <a href='https://github.com/ChanduRoyalA'><AiFillGithub style={{ fontSize: '18px' }} /></a>
+                        <a href='https://github.com/DineshAnnamreddy'><AiFillGithub style={{ fontSize: '18px' }} /></a>
                     </div>
                 </div>
             }
